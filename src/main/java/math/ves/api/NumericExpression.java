7105e93101c635cc92aa074e7d9a8a6ccb1c336e
Matheus Valero <matheusvalero99@gmail.com>
2023-10-16 21:02:11 -0300

    Update

--- src/main/java/math/ves/api/NumericExpression.java
+++ src/main/java/math/ves/api/NumericExpression.java
@@ -1,56 +1,59 @@
 package math.ves.api;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 import java.util.Random;
 
+@RestController
+@RequestMapping("/numericexpression")
 public class NumericExpression {
-    private int numero1;
-    private int numero2;
-    private int numero3;
-    private int numero4;
-    private int numero5;
-    private float numero6;
-    private float numero7;
+    @PostMapping
+    public static int gerarExpressao(@RequestBody ) {
 
-    public int gerarExpressao() {
         Random random = new Random();
         int numeroAleatorio = random.nextInt(30) + 1;
         int expressao;
+
+        int[] arrayInt = new int[10];
+        double[] arrayDouble = new double[10];
+
+        int numeroInt;
+        double numeroDouble;
+        for(int i = 0; i < 10; i++){
+            numeroInt = random.nextInt(30) + 1;
+            numeroDouble = random.nextDouble(30) + 1;
+            arrayInt[i] = numeroInt;
+            arrayDouble[i] = numeroDouble;
+        }
         while (true) {
             try {
                 switch (numeroAleatorio) {
                     case 1:
-                        expressao = numero1 + numero2 * (numero3 / numero4)
-                                + (int) Math.pow(numero6, numero7);
+                        expressao = arrayInt[4] + arrayInt[2] * (arrayInt[7] / arrayInt[9])
+                                + (int) Math.pow(arrayDouble[5], arrayDouble[8]);
+
+                        System.out.println(arrayInt[4] + " + " + arrayInt[2] + " * (" + arrayInt[7]
+                                + " / " + arrayInt[9] + ") + " + arrayDouble[5] + "^" + arrayDouble[8]);
                         return expressao;
 
                     case 2:
-                        expressao = numero1 / numero2 + (numero3 / numero4)
-                                + (int) Math.pow(numero6, numero7) * numero5;
+                        expressao = arrayInt[4] + arrayInt[2] * (arrayInt[7] / arrayInt[9])
+                                + (int) Math.pow(arrayDouble[5], arrayDouble[8]) + arrayInt[1];
                         return expressao;
 
                     case 3:
-                        expressao = (int) Math.pow(numero6, numero7) -
-                                numero1 / numero2 + (numero3 / numero4)
-                                + numero5;
+                        expressao = (int) Math.pow(arrayDouble[3], arrayDouble[6]) -
+                                arrayInt[1] / arrayInt[8] + (arrayInt[2] / arrayInt[5])
+                                + arrayInt[9];
                         return expressao;
                 }
             } catch (Exception e) {
                 return 0;
             }
         }
-    }
-
-    private int gerarNumeroBaseInteiro() {
-        Random random = new Random();
-        int numero;
-        numero = random.nextInt(30) + 1;
-        return numero;
-    }
-
-    private float gerarNumeroBaseFlutuante() {
-        Random random = new Random();
-        float numero;
-        numero = random.nextFloat(20) + 1;
-        return numero;
     }
 }
